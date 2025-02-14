765546ce5e87f914ac429fed1a6d2729fed2773b
Hira-shi <carreljeremie@gmail.com>
2025-02-14 22:55:26 +0100

    J'ai tout cassé

---
+++ build/CMakeFiles/Morpions.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Morpions.dir/src/board.cpp.obj"
+  "CMakeFiles/Morpions.dir/src/board.cpp.obj.d"
+  "CMakeFiles/Morpions.dir/src/game.cpp.obj"
+  "CMakeFiles/Morpions.dir/src/game.cpp.obj.d"
+  "CMakeFiles/Morpions.dir/src/main.cpp.obj"
+  "CMakeFiles/Morpions.dir/src/main.cpp.obj.d"
+  "CMakeFiles/Morpions.dir/src/rules.cpp.obj"
+  "CMakeFiles/Morpions.dir/src/rules.cpp.obj.d"
+  "Morpions.exe"
+  "Morpions.exe.manifest"
+  "Morpions.pdb"
+  "libMorpions.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Morpions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Morpions.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Morpions.
+# This may be replaced when dependencies are built.
